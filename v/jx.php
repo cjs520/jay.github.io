402cdc94daf8c55129ab283482d582b1fc8a1509
Jayson <cjszxzx520@163.com>
2020-02-25 19:37:12 +0800

    First commit

---
+++ v/jx.php
@@ -0,0 +1,151 @@
+<html lang="zh-CN">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="renderer" content="webkit">
+    <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
+    <meta name="theme-color" content="#000000">
+    <link rel="shortcut icon" href="data:image/ico;base64,aWNv" type="image/x-icon">
+    <!--[if lt IE 11]>
+    <script>
+        alert('您使用的浏览器版本过低，很多功能无法使用，建议您使用火狐 QQ 360等非ie浏览器访问')
+    </script>
+    <![endif]--><title></title>
+    <style>.lds-ellipsis {
+            display: inline-block;
+            position: relative;
+            width: 64px;
+            height: 64px
+        }
+
+        .lds-ellipsis div {
+            position: absolute;
+            top: 27px;
+            width: 11px;
+            height: 11px;
+            border-radius: 50%;
+            box-shadow: 1px 1px 2px #000;
+            animation-timing-function: cubic-bezier(0, 1, 1, 0)
+        }
+
+        .lds-ellipsis div:nth-child(1) {
+            left: 6px;
+            background: red;
+            animation: lds-ellipsis1 .6s infinite
+        }
+
+        .lds-ellipsis div:nth-child(2) {
+            left: 6px;
+            background: #93a2ff;
+            animation: lds-ellipsis2 .6s infinite
+        }
+
+        .lds-ellipsis div:nth-child(3) {
+            left: 26px;
+            background: rgba(255, 255, 255, .76);
+            animation: lds-ellipsis2 .6s infinite
+        }
+
+        .lds-ellipsis div:nth-child(4) {
+            left: 45px;
+            background: #fff;
+            animation: lds-ellipsis3 .6s infinite
+        }
+
+        @keyframes lds-ellipsis1 {
+            0% {
+                transform: scale(0)
+            }
+            100% {
+                transform: scale(1)
+            }
+        }
+
+        @keyframes lds-ellipsis3 {
+            0% {
+                transform: scale(1)
+            }
+            100% {
+                transform: scale(0)
+            }
+        }
+
+        @keyframes lds-ellipsis2 {
+            0% {
+                transform: translate(0, 0)
+            }
+            100% {
+                transform: translate(19px, 0)
+            }
+        }
+
+        .l {
+            display: inline-block;
+            color: #fff;
+            text-shadow: 1px 1px 2px #000;
+            font-size: .5em;
+            position: absolute;
+            left: 0;
+            right: 0;
+            top: 0;
+            bottom: 0;
+            width: 30em;
+            height: 7em;
+            margin: auto;
+            text-align: center;
+            z-index: 9
+        }
+
+        .hide {
+            display: none
+        }
+
+        #refresh {
+            display: none;
+            font-size: 1.5em;
+            padding: .2em 1em;
+            background: red;
+            border-radius: .1em;
+            margin-top: .5em;
+            cursor: pointer;
+            box-shadow: 0 0 1px #000;
+            text-shadow: none
+        }</style>
+    <script type="text/javascript" async="" charset="utf-8"
+            src="./core.php?web_id=1274263051&amp;t=z"></script>
+    <script charset="utf-8" src="js/UrlChangeTracker.js"></script>
+    <script>function c1(u) {
+            let h = document.getElementsByTagName('head')[0];
+            let l = document.createElement('link');
+            l.rel = 'stylesheet';
+            l.href = u;
+            h.appendChild(l)
+        }
+        function j1(u) {
+            let h = document.getElementsByTagName('head')[0];
+            let s = document.createElement('script');
+            s.src = u;
+            h.appendChild(s)
+        }</script>
+    <link href="hcss/main.d2895680.css" onerror="c1('/css/main.d2895680.css')"
+          rel="stylesheet">
+    <script src="./z_stat.php"></script>
+</head>
+<body bgcolor="black">
+<div class="l hide">
+    <div class="lds-ellipsis">
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+    <div id="l-text" style="margin-top:-1.5em">正在加载视频</div>
+    <div id="l-refresh">(若长时间加载不出来请尝试刷新页面)</div>
+    <div id="refresh" onclick="location.reload()" style="display: inline-block;">刷新</div>
+</div>
+<noscript></noscript>
+
+<script>window.mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)</script>
+<script type="text/javascript" onerror="j1('js/main.b6956688.js')"
+        src="js/main.b6956688.js"></script>
+</body>
+</html>
